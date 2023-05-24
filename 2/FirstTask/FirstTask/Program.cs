b5cd4ddb5d6ae6bfac4e965bdd5ebe21542d1ee5
Hans Christopher Linchpin <misha.sytch@mail.ru>
2023-05-24 20:19:27 +0300

    Второе задание на многопоточность с текстом в миллион слов)))

---
+++ 2/FirstTask/FirstTask/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Program
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите директроию файла (сепаратор = '/'): ");
+            string path = Console.ReadLine().Trim();
+            Console.Write("Введите имя файла с разрешением: ");
+            string name = Console.ReadLine().Trim();
+
+            using (StreamReader streamReader = new StreamReader($"{path}/{name}"))
+            {
+                StringBuilder _text = new StringBuilder();
+                string line = "";
+                while ((line = (streamReader.ReadLine())) != null)
+                {
+                    _text.Append(line).Append(" ");
+                }
+
+                WordCounter.WordCounter wordCounter = new WordCounter.WordCounter();
+                var reflectMeth = wordCounter.GetType().GetMethod("Count", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                Dictionary<string, int> dict = (Dictionary<string, int>)reflectMeth.Invoke(wordCounter, new object[] {_text.ToString()});
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                string notParallelTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+
+                wordCounter = new WordCounter.WordCounter();
+                stopWatch = new Stopwatch();
+                stopWatch.Start();
+                dict = wordCounter.ParallelStart(_text.ToString()); 
+                stopWatch.Stop();
+                ts = stopWatch.Elapsed;
+                string parallelTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+                
+                using (StreamWriter streamWriter = File.CreateText($"{path}/time.txt"))
+                {
+                    streamWriter.WriteLine($"Parallel Time: {parallelTime}\n");
+                    streamWriter.WriteLine($"Not Parallel Time: {notParallelTime}");
+                }
+                StringBuilder str = new StringBuilder();
+                foreach (string word in dict.Keys) str.AppendLine(String.Format("{0,-24} {1}", word, dict[word]));
+                using (StreamWriter streamWriter = File.CreateText($"{path}/count.txt"))
+                {
+                    streamWriter.WriteLine(str);
+                }
+            }
+        }
+    }
+}
---
+++ 2/WordCounter/WordCounter/WordCounter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WordCounter
+
+{
+    public class WordCounter
+    {
+        private Dictionary<string, int> _words = new Dictionary<string, int>();
+        private object _lock = new object();
+
+        private Dictionary<string, int> Count(string _text)
+        {
+            string[] strings = _text.Trim().Split(' ');
+            for (int i = 0; i < strings.Length; i++)
+            {
+                string tmp = strings[i];
+
+                string variable = "";
+                foreach (var letter in tmp)
+                    if (char.IsLetter(letter) || letter.Equals("'")) //Регулярку вставить бы
+                        variable += letter;
+                if (variable.Trim().Length == 0) continue;
+                if (_words.ContainsKey(variable))
+                {
+                    _words[variable]++;
+                }
+                else
+                {
+                    _words.Add(variable, 1);
+                }
+            }
+            return _words;
+        }
+        public Dictionary<string, int> ParallelStart(string _text)
+        {
+            Parallel.ForEach(_text.Split(' '), PCount);
+
+            return _words;
+        }
+
+
+        private void PCount(string word)
+        {
+            lock (_lock)
+            {
+                string variable = "";
+                foreach (var letter in word)
+                    if (char.IsLetter(letter) || letter.Equals("'")) //Регулярку вставить бы
+                        variable += letter;
+                if (variable.Trim().Length != 0)
+                {
+                    if (_words.ContainsKey(variable))
+                    {
+                        _words[variable]++;
+                    }
+                    else
+                    {
+                        _words.Add(variable, 1);
+                    }
+                }
+            }
+        }
+    }
+}
