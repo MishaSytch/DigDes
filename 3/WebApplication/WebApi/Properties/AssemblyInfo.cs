2105ef87e6fc917efe516ef4031df5e91587b07f
Hans Christopher Linchpin <misha.sytch@mail.ru>
2023-05-26 19:44:49 +0300

    Задание на веб-сервисы добавлено, но есть ньанс с выводом. В файле README.md папки "3" все описано. Технически - задание выполнено, но без предворительной обработки словаря, оно работать не будет (если мы хотели просто вывести словарь в браузер)

---
+++ 3/WebApplication/WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("WebApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApi")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("52306874-ebeb-4ab8-9f7e-8d8e6d8c68a1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии 
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 3/WordCounter/WordCounter/WordCounter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WordCounter
+
+{
+    public class WordCounter
+    {
+        private static Dictionary<string, int> _words = new Dictionary<string, int>();
+        private object _lock = new object();
+
+        private Dictionary<string, int> Count(string _text)
+        {
+            string[] strings = _text.Trim().Split(' ');
+            for (int i = 0; i < strings.Length; i++)
+            {
+                string tmp = strings[i];
+
+                StringBuilder variable = new StringBuilder();
+                foreach (var letter in tmp)
+                    if (char.IsLetter(letter) || letter.Equals("'") || letter.Equals("-") && variable.ToString().Length != 0 ) //Регулярку вставить бы
+                        variable.Append(letter);
+                if (variable.ToString().Trim().Length == 0) continue;
+                if (_words.ContainsKey(variable.ToString()))
+                {
+                    _words[variable.ToString()]++;
+                }
+                else
+                {
+                    _words.Add(variable.ToString(), 1);
+                }
+            }
+            return _words;
+        }
+        public Dictionary<string, int> ParallelStart(string _text)
+        {
+            Parallel.ForEach(_text.Split(' '), PCount);
+
+            return _words;
+        }
+
+
+        private void PCount(string word)
+        {
+            //lock (_lock)
+            {
+                StringBuilder variable = new StringBuilder();
+                foreach (var letter in word)
+                    if (char.IsLetter(letter) || letter.Equals("'") || letter.Equals("-") && variable.ToString().Length != 0) //Регулярку вставить бы
+                        variable.Append(letter);
+                if (variable.ToString().Trim().Length != 0)
+                {
+                    if (_words.ContainsKey(variable.ToString()))
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                _words[variable.ToString()]++;
+                                break;
+                            } catch (Exception e) { }
+                            
+                        }
+                    }
+                    else
+                    {
+                        _words.Add(variable.ToString(), 1);
+                    }
+                }
+            }
+        }
+    }
+}
